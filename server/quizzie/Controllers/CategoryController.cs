6e66dcdff11d473b94332f837394661f175d5f15
haydox01 <oladimejiabdullahi04@gmail.com>
2024-01-03 16:35:38 +0100

    Created Get Category By Id End Point

--- server/quizzie/Controllers/CategoryController.cs
+++ server/quizzie/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using quizzie;
 using Quizzie.Data;
@@ -23,6 +24,7 @@ public class CategoryController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Roles ="Admin")]
     public async Task<ActionResult> CreateCategory([FromBody] CategoryDto categoryDto)
     {
         var existingCategory = await _categoryRepository.GetByTitle(categoryDto.Title);
@@ -50,4 +52,12 @@ public class CategoryController : ControllerBase
         });
     }
 
+    [HttpGet("{id:Guid}")]
+    [Authorize(Roles ="Admin")]
+
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        var category = await _categoryRepository.GetById(id);
+        return Ok(category);
+    }
 }
