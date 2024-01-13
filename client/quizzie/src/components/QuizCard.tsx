5540651d2c3e4d37a606e659f6c90f9036594cc7
tishly <ekaetteessien27@gmail.com>
2024-01-13 11:46:55 +0000

    Created the Search bar component and exported to the user dashboard

--- client/quizzie/src/components/QuizCard.tsx
+++ client/quizzie/src/components/QuizCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface Quiz {
     quizName: string;
@@ -8,12 +9,14 @@ interface Quiz {
 function QuizCard(props: Quiz) {
     return (
         <div>
-            <div className='bg-white flex justify-between w-1/3 border p-4 rounded-lg'>
-                <div className='flex flex-col md:w-1/3 pb-4'>
-                    <img src={props.quizPicture} alt="Quiz Picture" />
-                    <p className='text-gray-600'>{props.quizName}</p>
+            <Link href="">
+                <div className='bg-white flex justify-between w-full border p-2 rounded-lg'>
+                    <div className='flex flex-col md:w-full pb-4'>
+                        <img src={props.quizPicture} alt="Quiz Picture" />
+                        <p className='text-gray-600'>{props.quizName}</p>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
